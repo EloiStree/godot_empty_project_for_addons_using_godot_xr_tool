a03136a6893543858e8b89a85f92ee1139ad5959
Eloi Stree <eloistree@gmail.com>
2026-05-29 06:48:27 +0200

    init

---
+++ xr_setup_of_manual.gd
@@ -0,0 +1,13 @@
+## Source https://docs.godotengine.org/en/stable/tutorials/xr/setting_up_xr.html
+extends Node
+
+var xr_interface: XRInterface
+
+func _ready():
+	xr_interface = XRServer.find_interface("OpenXR")
+	if xr_interface and xr_interface.is_initialized():
+		print("OpenXR initialized successfully")
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		get_viewport().use_xr = true
+	else:
+		print("OpenXR not initialized, please check if your headset is connected")
